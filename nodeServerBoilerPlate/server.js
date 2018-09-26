8427577c389343c5f9931a1f73cf0c1c86cbdff4
Victor <victorevangelista@protonmail.com>
2018-09-26 04:16:45 -0700

    establish base mongodb state

--- nodeServerBoilerPlate/server.js
+++ nodeServerBoilerPlate/server.js
@@ -1,6 +1,32 @@
+'use strict';
+
 const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
+
+const mongoose = require('mongoose');
+
+const User = require('./models/User');
+const makeData = require('./config/users');
+
+mongoose.connect('mongodb://localhost:27017/unipower', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on('error', err => console.error(`connection error: ${err}`) );
+db.once('open', async () => {
+  console.log('db connection successful');
+  await User.deleteMany(err => err ? console.error(err) : console.log('all user docs deleted'));
+  
+  const initialData = makeData();
+  const duplicateUserData = 
+    [...initialData, initialData]
+    .map(user => new User(user));
+
+  User.collection.insertMany(duplicateUserData, (err) => {
+    if (err) console.error(err);
+    else console.log('users inserted');
+  });
+});
 
 const app = express();
 
