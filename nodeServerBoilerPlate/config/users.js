8c93a0caac258da796d1d865c48d1acd84cf791b
Victor <victorevangelista@protonmail.com>
2018-09-26 04:09:57 -0700

    modularize random user data creator

--- nodeServerBoilerPlate/config/users.js
+++ nodeServerBoilerPlate/config/users.js
@@ -1,9 +1,11 @@
+'use strict';
+
 const _ = require('lodash')
 const Chance = require('chance')
 const chance = new Chance('seed')
 
 // Generates random user data
-const range = len => {
+const _range = len => {
   const arr = []
   for (let i = 0; i < len; i += 1) {
     arr.push(i)
@@ -11,7 +13,7 @@ const range = len => {
   return arr
 }
 
-const newPerson = () => {
+const _newPerson = () => {
   const statusChance = Math.random()
   return {
     id: Math.round(Math.random() * (1000 - 10) + 10),
@@ -24,16 +26,6 @@ const newPerson = () => {
   }
 }
 
-const makeData = (len = 80) => range(len).map(d => newPerson())
+const makeData = (len = 80) => _range(len).map(d => _newPerson())
 
-const initialData = makeData()
-
-// NOTE: ignore this for now. But we will need to filter duplicates on the front end.
-const userData = [...initialData, ...initialData]
-
-module.exports = {
-  // YOUR CODE GOES HERE
-  // 1. Make a function to retrieve all Users
-  // 2. Make a function to retrieve Users given a string to match against names
-  // 3. Make a function to add a new User
-}
+module.exports = makeData;
