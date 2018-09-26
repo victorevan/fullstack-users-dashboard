b979f69c4a0aa3d5e291f1167f13f4b167c2cb23
Victor <victorevangelista@protonmail.com>
2018-09-26 02:19:21 -0700

    implement error handling

--- nodeServerBoilerPlate/server.js
+++ nodeServerBoilerPlate/server.js
@@ -7,4 +7,19 @@ app.use(cors())
 
 require('./config/middleware')(app, express)
 
+app.use((req, res, next) => {
+	var err = new Error("Not Found");
+	err.status = 404;
+	next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
 app.listen(8000, () => console.log('listening on 8000'))
