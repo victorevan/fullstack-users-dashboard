e703183f3617f638caa7c946906c308fbde70062
Victor <victorevangelista@protonmail.com>
2018-09-26 18:14:41 -0700

    setup base routes and controllers

---
+++ nodeServerBoilerPlate/controllers/userController.js
@@ -0,0 +1,25 @@
+const User = require('../models/User');
+
+exports.read_user_list = (req, res) => {
+
+};
+
+exports.create_user = (req, res) => {
+
+};
+
+exports.preload_user_doc = (req, res, next, userId) => {
+
+};
+
+exports.read_user = (req, res) => {
+
+};
+
+exports.update_user = (req, res) => {
+
+};
+
+exports.delete_user = (req, res) => {
+
+};
--- nodeServerBoilerPlate/routes/api.js
+++ nodeServerBoilerPlate/routes/api.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const user_controller = require('../controllers/userController');
+
+router.param('userId', user_controller.preload_user_doc);
+
+router.route('/users')
+  .get(user_controller.read_user_list)
+  .post(user_controller.create_user)
+
+router.route('/users/:userId')
+  .get(user_controller.read_user)
+  .put(user_controller.update_user)
+  .delete(user_controller.delete_user)
+
 module.exports = router;
