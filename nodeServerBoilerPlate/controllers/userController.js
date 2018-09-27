6cee6ff168f2829aa0507c24fdc6b0c25a83fe62
Victor <victorevangelista@protonmail.com>
2018-09-27 16:29:28 -0700

    implement /api/users/:userId controllers

--- nodeServerBoilerPlate/controllers/userController.js
+++ nodeServerBoilerPlate/controllers/userController.js
@@ -21,18 +21,39 @@ exports.create_user = async (req, res, next) => {
   }
 };
 
-exports.preload_user_doc = (req, res, next, userId) => {
-
+exports.preload_user_doc = async (req, res, next, userId) => {
+  try {
+    const userDoc = await User.findById(userId).exec();
+    if (!userDoc) {
+      err = new Error("Not Found");
+      err.status = 404;
+      return next(err);
+    }
+    req.userDoc = userDoc;
+    next();
+  } catch(err) {
+    err.status = 400;
+    next(err);
+  }
 };
 
-exports.read_user = (req, res) => {
+exports.read_user = (req, res) => res.json(req.userDoc);
 
+exports.update_user = async (req, res, next) => {
+  try {
+    const updatedStatus = await req.userDoc.updateOne(req.body).exec();
+    res.json(updatedStatus);
+  } catch(err) {
+    err.status = 400;
+    next(err);
+  }
 };
 
-exports.update_user = (req, res) => {
-
-};
-
-exports.delete_user = (req, res) => {
-
+exports.delete_user = async (req, res, next) => {
+  try {
+    const deletedStatus = await req.userDoc.remove();
+    res.json(deletedStatus);
+  } catch(err) {
+    next(err);
+  }
 };
