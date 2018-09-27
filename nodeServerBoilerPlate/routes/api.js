cc058a1190f6a7fcce717a3c7d2683cc21cdf45b
Victor <victorevangelista@protonmail.com>
2018-09-27 16:27:17 -0700

    reorganize basic logic

--- nodeServerBoilerPlate/routes/api.js
+++ nodeServerBoilerPlate/routes/api.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 const user_controller = require('../controllers/userController');
 
-router.param('userId', user_controller.preload_user_doc);
-
 router.route('/users')
   .get(user_controller.read_user_list)
-  .post(user_controller.create_user)
+  .post(user_controller.create_user);
+
+router.param('userId', user_controller.preload_user_doc);
 
 router.route('/users/:userId')
   .get(user_controller.read_user)
   .put(user_controller.update_user)
-  .delete(user_controller.delete_user)
+  .delete(user_controller.delete_user);
 
 module.exports = router;
