02a471d96a22a8f949a7a9de36068461ae18dbca
Victor <victorevangelista@protonmail.com>
2018-09-28 22:33:16 -0700

    remove custom logger

--- REACTSTARTER/src/middleware/index.js
+++ REACTSTARTER/src/middleware/index.js
@@ -2,11 +2,10 @@ import thunk from 'redux-thunk';
 import { applyMiddleware, compose } from 'redux';
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
-import logger from './logger';
 
 export const history = createBrowserHistory();
 
 // eslint-disable-next-line
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default composeEnhancers(applyMiddleware(thunk, routerMiddleware(history), logger));
+export default composeEnhancers(applyMiddleware(thunk, routerMiddleware(history)));
