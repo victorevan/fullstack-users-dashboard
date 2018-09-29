9f3fdf30339c6e99c3c4f423462ea109641ce36b
Victor <victorevangelista@protonmail.com>
2018-09-28 23:52:32 -0700

    update handleInitialData import

--- REACTSTARTER/src/components/App/index.js
+++ REACTSTARTER/src/components/App/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import handleInitialData from '../../thunk/handleInitialData';
+import handleInitialData from '../../actions/shared';
 
 class App extends Component {
   static propTypes = {
