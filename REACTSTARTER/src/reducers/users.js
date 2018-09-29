1bcf2d606d7bcb969f514fa095a4215a3d8a7054
Victor <victorevangelista@protonmail.com>
2018-09-29 00:03:41 -0700

    begin implementing save new user reducer case

--- REACTSTARTER/src/reducers/users.js
+++ REACTSTARTER/src/reducers/users.js
@@ -1,4 +1,4 @@
-import { RECEIVE_USERS } from '../actions/users';
+import { RECEIVE_USERS, ADD_USER } from '../actions/users';
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -7,6 +7,11 @@ export default function users(state = {}, action) {
         ...state,
         ...action.users,
       };
+    // TODO
+/*     case ADD_USER:
+      return {
+        ...state,
+      }; */
     default:
       return state;
   }
