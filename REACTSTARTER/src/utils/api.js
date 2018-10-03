fb6207b0112fa55f8397e172c51fe465068cf802
Victor <victorevangelista@protonmail.com>
2018-10-02 18:08:40 -0700

    upgrade api utils to fully utilize endpoints

--- REACTSTARTER/src/utils/api.js
+++ REACTSTARTER/src/utils/api.js
@@ -8,7 +8,7 @@ const serverBaseUrl = 'http://localhost:8000';
 export async function getAllUserData() {
   const response = await fetch(`${serverBaseUrl}/api/users`);
   const usersArray = await response.json();
-  return usersArray;
+  return Promise.resolve(usersArray);
 }
 
 export async function getPaginationData(queries = null) {
@@ -18,7 +18,7 @@ export async function getPaginationData(queries = null) {
   }
   const response = await fetch(`${serverBaseUrl}/api/users/paginated?${queryOptions}`);
   const paginationData = await response.json();
-  return paginationData;
+  return Promise.resolve(paginationData);
 }
 
 export async function saveUser(user) {
@@ -28,7 +28,32 @@ export async function saveUser(user) {
     body: JSON.stringify(user),
   });
   const userObject = await response.json();
-  return userObject;
+  return Promise.resolve(userObject);
+}
+
+export async function getUser(id) {
+  const response = await fetch(`${serverBaseUrl}/api/users/${id}`);
+  const user = await response.json();
+  return Promise.resolve(user);
+}
+
+export async function editUser(id, updates) {
+  const response = await fetch(`${serverBaseUrl}/api/users/${id}`, {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(updates),
+  });
+  const status = await response.json();
+  return Promise.resolve(status);
+}
+
+export async function deleteUser(id) {
+  const response = await fetch(`${serverBaseUrl}/api/users/${id}`, {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  const deletedUser = await response.json();
+  return Promise.resolve(deletedUser);
 }
 
 /* eslint-enable no-undef */
