7e58bbc13dc9d73be59db53105291554ebd977fd
Victor <victorevangelista@protonmail.com>
2018-10-02 18:15:03 -0700

    rename App.js to index.js

--- REACTSTARTER/src/components/App/index.js
+++ REACTSTARTER/src/components/App/index.js
@@ -3,13 +3,19 @@ import { Container } from 'semantic-ui-react';
 import { Switch, Route } from 'react-router-dom';
 import LoadingBar from 'react-redux-loading';
 
-import Dashboard from '../Dashboard/Dashboard';
+import Navbar from '../Navbar';
+import Dashboard from '../Dashboard';
+import User from '../User';
+import AddUser from '../AddUser';
 
 const App = () => (
   <Container>
     <LoadingBar />
+    <Navbar />
     <Switch>
       <Route path="/" exact component={Dashboard} />
+      <Route path="/user/:id" component={User} />
+      <Route path="/add" component={AddUser} />
       <Route render={() => <h1>Not Found</h1>} />
     </Switch>
   </Container>
