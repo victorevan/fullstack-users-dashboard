115370903bfffdbf8c3447604f4b711f6bf5c53a
Victor <victorevangelista@protonmail.com>
2018-10-02 18:13:37 -0700

    update SearchInput props

--- REACTSTARTER/src/components/Dashboard/SearchInput.js
+++ REACTSTARTER/src/components/Dashboard/SearchInput.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Input, Dropdown } from 'semantic-ui-react';
 
 const SearchInput = ({
-  changeSearch, changeFilter, search, options, currentFilter,
+  changeSearch, changeFilter, search, filterOptions, currentFilter,
 }) => (
   <Input type="text" placeholder="Search by..." action>
     <input
@@ -12,31 +12,33 @@ const SearchInput = ({
     />
     <Dropdown
       selection
-      options={options}
+      options={filterOptions}
       value={currentFilter}
       onChange={changeFilter}
     />
   </Input>
 );
 
+SearchInput.defaultProps = {
+  filterOptions: [
+    { key: 'name', text: 'Name', value: 'name' },
+    { key: 'role', text: 'Role', value: 'role' },
+    { key: 'location', text: 'Location', value: 'location' },
+    { key: 'type', text: 'Type', value: 'type' },
+    { key: 'surveyStatus', text: 'Survey Status', value: 'surveyStatus' },
+  ],
+};
+
 SearchInput.propTypes = {
   search: PropTypes.string.isRequired,
   currentFilter: PropTypes.string.isRequired,
   changeSearch: PropTypes.func.isRequired,
   changeFilter: PropTypes.func.isRequired,
-  options: PropTypes.arrayOf(PropTypes.shape({
+  filterOptions: PropTypes.arrayOf(PropTypes.shape({
     key: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
   })),
-};
-
-SearchInput.defaultProps = {
-  options: [
-    { key: 'name', text: 'Name', value: 'name' },
-    { key: 'role', text: 'Role', value: 'role' },
-    { key: 'location', text: 'Location', value: 'location' },
-  ],
 };
 
 export default SearchInput;
